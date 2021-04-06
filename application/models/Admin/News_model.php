2f3f0f76494aa0f2e8eed2979690f2dd67cb2acd
hoangloc1307 <hoangloc1307@gmail.com>
2021-04-06 10:44:44 +0700

    them danh muc bai viet o home

--- application/models/Admin/News_model.php
+++ application/models/Admin/News_model.php
@@ -4,20 +4,33 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class News_model extends CI_Model
 {
-    public function getNews($link = null)
+
+    /* Hàm lấy bài viết:
+    1 - Trả về tất cả bài viết nếu không chỉ định $link.
+    2 - Trả về bài viết với link được chỉ định nếu không chỉ định $category.
+    3 - Trả về danh sách các bài viết có cùng danh mục nếu cả hai biến được chỉ định.
+    */
+    public function GetNews($link = null, $category = null)
     {
         if (is_null($link)) {
             $query = $this->db->get('tbl_news');
             return $query->result_array();
+        } else {
+            if (is_null($category)) {
+                $where = "LinkCustom = '" . $link . "' OR LinkDefault = '" . $link . "'";
+                $this->db->where($where);
+                $query = $this->db->get('tbl_news');
+                return $query->row_array();
+            } else {
+                $this->db->where('Category', $category);
+                $query = $this->db->get('tbl_news');
+                return $query->result_array();
+            }
         }
-
-        $where = "LinkCustom = '" . $link . "' OR LinkDefault = '" . $link . "'";
-        $this->db->where($where);
-        $query = $this->db->get('tbl_news');
-        return $query->row_array();
     }
 
-    public function addNews($name, $description, $content, $image, $linkcustom)
+    //Hàm thêm bài viết mới.
+    public function AddNews($name, $description, $content, $image, $linkcustom)
     {
         date_default_timezone_set('Etc/GMT+5');
         $datetime = date("Y-m-d h:i:s");
@@ -37,21 +50,27 @@ class News_model extends CI_Model
         return $this->db->insert_id();
     }
 
-    public function update($id, $view)
+    //Hàm cập nhật lượt xem bài viết.
+    public function UpdateView($id, $view)
     {
         $this->db->where('News_ID', $id);
         $data = ['View' => $view];
         $this->db->update('tbl_news', $data);
     }
 
-    public function getNewsCategory($id = NULL)
+
+    /*Hàm lấy danh mục bài viết
+    1 - Trả về tát cả danh mục nếu không chỉ định $link.
+    2 - Trả về danh mục có link được chỉ định nếu chỉ định $link.
+    */
+    public function GetNewsCategory($link = NULL)
     {
-        if (is_null($id)) {
+        if (is_null($link)) {
             $query = $this->db->get('tbl_news_category');
             return $query->result_array();
         }
 
-        $this->db->where($id, 'News_Category_ID ');
+        $this->db->where('Link', $link);
         $query = $this->db->get('tbl_news_category');
         return $query->row_array();
     }
--- application/views/home/news_detail.php
+++ application/views/home/news_detail.php
@@ -1,6 +1,6 @@
 <?php /*?>
 
-<h2><?= $new['Name'] ?></h2>
+<h2><?= $news['Name'] ?></h2>
 <img src="<?= $new['Image'] ?>" alt="">
 <p><?= $new['Description'] ?></p>
 <p><?= $new['Content'] ?></p>
@@ -11,56 +11,56 @@
 */ ?>
 
 <section id="news-detail">
-	<div class="grid wide">
-		<div class="row">
-			<div class="col l-8 m-8 c-12 news-detail">
-				<h2 class="title">
-					<?= $new['Name'] ?>
-				</h2>
-				<div class="detail">
-					<?= $new['Content'] ?>
-				</div>
-				<p class="date">
-					<?= $new['Date'] ?>
-				</p>
-			</div>
-			<div class="col l-4 m-4 c-12 news-related">
-				<div class="news-related-wrap">
-					<h4 class="related-title">
-						Tin tức liên quan
-					</h4>
-					<ul class="related-list">
-						<li class="related-item">
-							<a href="#" class="related-thumb">
-								<img src="<?= base_url() ?>assets/images/favorite/hoian.jpg" alt="">
-							</a>
-							<h4 class="related-heading">
-								<a href="#" target="_blank" title="">Tư vấn hỗ trợ đặt vé Tư vấn hỗ trợ đặt
-									vé</a>
-							</h4>
-						</li>
-						<li class="related-item">
-							<a href="#" class="related-thumb">
-								<img src="<?= base_url() ?>assets/images/favorite/hoian.jpg" alt="">
-							</a>
-							<h4 class="related-heading">
-								<a href="#" target="_blank" title="">Tư vấn hỗ trợ đặt vé Tư vấn hỗ trợ đặt
-									vé</a>
-							</h4>
-						</li>
-						<li class="related-item">
-							<a href="#" class="related-thumb">
-								<img src="<?= base_url() ?>assets/images/favorite/hoian.jpg" alt="">
-							</a>
-							<h4 class="related-heading">
-								<a href="#" target="_blank" title="">Tư vấn hỗ trợ đặt vé Tư vấn hỗ trợ đặt
-									vé</a>
-							</h4>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="grid wide">
+        <div class="row">
+            <div class="col l-8 m-8 c-12 news-detail">
+                <h2 class="title">
+                    <?= $news['Name'] ?>
+                </h2>
+                <div class="detail">
+                    <?= $news['Content'] ?>
+                </div>
+                <p class="date">
+                    <?= $news['Date'] ?>
+                </p>
+            </div>
+            <div class="col l-4 m-4 c-12 news-related">
+                <div class="news-related-wrap">
+                    <h4 class="related-title">
+                        Tin tức liên quan
+                    </h4>
+                    <ul class="related-list">
+                        <li class="related-item">
+                            <a href="#" class="related-thumb">
+                                <img src="<?= base_url() ?>assets/images/favorite/hoian.jpg" alt="">
+                            </a>
+                            <h4 class="related-heading">
+                                <a href="#" target="_blank" title="">Tư vấn hỗ trợ đặt vé Tư vấn hỗ trợ đặt
+                                    vé</a>
+                            </h4>
+                        </li>
+                        <li class="related-item">
+                            <a href="#" class="related-thumb">
+                                <img src="<?= base_url() ?>assets/images/favorite/hoian.jpg" alt="">
+                            </a>
+                            <h4 class="related-heading">
+                                <a href="#" target="_blank" title="">Tư vấn hỗ trợ đặt vé Tư vấn hỗ trợ đặt
+                                    vé</a>
+                            </h4>
+                        </li>
+                        <li class="related-item">
+                            <a href="#" class="related-thumb">
+                                <img src="<?= base_url() ?>assets/images/favorite/hoian.jpg" alt="">
+                            </a>
+                            <h4 class="related-heading">
+                                <a href="#" target="_blank" title="">Tư vấn hỗ trợ đặt vé Tư vấn hỗ trợ đặt
+                                    vé</a>
+                            </h4>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </section>
