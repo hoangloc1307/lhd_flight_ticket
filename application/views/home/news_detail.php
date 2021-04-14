922a66e540ad0f1bc32f085f54be18263e77432f
Nguyễn Trung Hiếu <hieucot69@gmail.com>
2021-04-14 11:04:22 +0700

    chinh menu

--- application/views/home/news_detail.php
+++ application/views/home/news_detail.php
@@ -1,39 +1,39 @@
 <section id="news-detail">
-    <div class="grid wide">
-        <div class="row">
-            <div class="col l-8 m-8 c-12 news-detail">
-                <h2 class="title">
-                    <?= $news['Name'] ?>
-                </h2>
-                <div class="detail">
-                    <?= $news['Content'] ?>
-                </div>
-                <p class="date">
-                    <?= $news['Date'] ?>
-                </p>
-            </div>
-            <div class="col l-4 m-4 c-12 news-related">
-                <div class="news-related-wrap">
-                    <h3 class="related-title">
-                        Bài viết liên quan
-                    </h3>
-                    <ul class="related-list">
-                        <?php foreach ($related_news as $item) : ?>
-                        <li class="related-item">
-                            <a href="<?= base_url() . 'news/' . ($item['LinkCustom'] != '' ? $item['LinkCustom'] : $item['LinkDefault']) ?>"
-                                class="related-thumb">
-                                <img src="<?= base_url() . $item['Image'] ?>">
-                            </a>
-                            <h4 class="related-heading">
-                                <a href="<?= base_url() . 'news/' . ($item['LinkCustom'] != '' ? $item['LinkCustom'] : $item['LinkDefault']) ?>"
-                                    title=""><?= $item['Name'] ?></a>
-                            </h4>
-                        </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="grid wide">
+		<div class="row">
+			<div class="col l-8 m-8 c-12 news-detail">
+				<h1 class="title">
+					<?= $news['Name'] ?>
+				</h1>
+				<div class="detail">
+					<?= $news['Content'] ?>
+				</div>
+				<p class="date">
+					<?= $news['Date'] ?>
+				</p>
+			</div>
+			<div class="col l-4 m-4 c-12 news-related">
+				<div class="news-related-wrap">
+					<h3 class="related-title">
+						Bài viết liên quan
+					</h3>
+					<ul class="related-list">
+						<?php foreach ($related_news as $item) : ?>
+						<li class="related-item">
+							<a href="<?= base_url() . 'news/' . ($item['LinkCustom'] != '' ? $item['LinkCustom'] : $item['LinkDefault']) ?>"
+								class="related-thumb">
+								<img src="<?= base_url() . $item['Image'] ?>">
+							</a>
+							<h4 class="related-heading">
+								<a href="<?= base_url() . 'news/' . ($item['LinkCustom'] != '' ? $item['LinkCustom'] : $item['LinkDefault']) ?>"
+									title=""><?= $item['Name'] ?></a>
+							</h4>
+						</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </section>
