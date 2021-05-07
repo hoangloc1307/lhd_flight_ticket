c8a246fb343c0f098168836dba44b925777eb7f4
Nguyễn Trung Hiếu <hieucot69@gmail.com>
2021-05-07 16:58:24 +0700

    them giao dien trang chi tiet hoa don

--- application/views/admin/order.php
+++ application/views/admin/order.php
@@ -1,103 +1,102 @@
-<h2>Danh sách hoá đơn</h3>
-	<section class="table order">
-		<div class="grid">
-
-			<div class="search-form">
-				<input type="text" placeholder="Tìm kiếm">
-				<button><i class="fas fa-search"></i></button>
+<h2>Danh sách hoá đơn</h2>
+<section class="table order">
+	<div class="grid">
+		<div class="search-form">
+			<input type="text" placeholder="Tìm kiếm">
+			<button><i class="fas fa-search"></i></button>
+		</div>
+		<div class="row heading">
+			<div class="col l-1">
+				<span>Mã code</span>
 			</div>
-			<div class="row heading">
+			<div class="col l-1">
+				<span>Số lượng</span>
+			</div>
+			<div class="col l-1">
+				<span>Giá</span>
+			</div>
+			<div class="col l-4">
+				<span>Hành trình</span>
+			</div>
+			<div class="col l-2">
+				<span>Thông tin liên hệ</span>
+			</div>
+			<div class="col l-2">
+				<span>Ngày đặt</span>
+			</div>
+			<div class="col l-1">
+				<span>Tình trạng</span>
+			</div>
+		</div>
+		<div class="items">
+			<?php
+			foreach ($orders as $item) :
+			$flight_detail = json_decode($item['Flight_Detail'], true);
+			$payment_info = json_decode($item['Payment_Info'], true);
+			?>
+			<a href="#" class="row body">
 				<div class="col l-1">
-					<span>Mã code</span>
+					<span><?= $item['Order_Code'] ?></span>
+				</div>
+				<div class="col l-1 amount">
+					<span><?php echo $payment_info['adults'] + (array_key_exists("children", $payment_info) ? $payment_info['children'] : 0) + (array_key_exists("infants", $payment_info) ? $payment_info['infants'] : 0) . ($item['Type'] == 'oneway' ? ' MC' : ' KH'); ?></span>
+					<p><?php echo $payment_info['adults'] . ' NL'; ?></p>
+					<p><?php echo (array_key_exists("children", $payment_info) ? $payment_info['children'] . ' TE' : ''); ?>
+					</p>
+					<p><?php echo (array_key_exists("infants", $payment_info) ? $payment_info['infants'] . ' EB' : ''); ?>
+					</p>
 				</div>
 				<div class="col l-1">
-					<span>Số lượng</span>
-				</div>
-				<div class="col l-1">
-					<span>Giá</span>
+					<span><?= number_format($payment_info['total_price'], 0, ".", ".") ?></span>
 				</div>
 				<div class="col l-4">
-					<span>Hành trình</span>
+					<?php foreach ($flight_detail['flight_detail'] as $flight) { ?>
+					<p><?= $flight['carrierCode'] . $flight['number'] . ' ' . $flight['date'] . ' ' . $flight['from'] . ' ' . $flight['to'] . ' ' . $flight['departure_time'] . ' ' . $flight['arrival_time'] ?>
+					</p>
+					<?php } ?>
 				</div>
 				<div class="col l-2">
-					<span>Thông tin liên hệ</span>
+					<p><?= $payment_info['contact_name'] ?></p>
+					<p><?= $payment_info['contact_phone'] ?></p>
 				</div>
 				<div class="col l-2">
-					<span>Ngày đặt</span>
+					<span><?= $item['Booking_DateTime'] ?></span>
 				</div>
-				<div class="col l-1">
-					<span>Tình trạng</span>
+				<div class="status col l-1">
+					<span payment="<?php echo $item['Status'] == 0 ? 'false' : 'true'; ?>"
+						value='<?= $item['Order_ID'] ?>'><?php echo $item['Status'] == 0 ? 'Chưa TT' : 'Đã TT'; ?></span>
 				</div>
-			</div>
-			<div class="items">
-				<?php
-                foreach ($orders as $item) :
-                    $flight_detail = json_decode($item['Flight_Detail'], true);
-                    $payment_info = json_decode($item['Payment_Info'], true);
-                ?>
-				<a href="#" class="row body">
-					<div class="col l-1">
-						<span><?= $item['Order_Code'] ?></span>
-					</div>
-					<div class="col l-1 amount">
-						<span><?php echo $payment_info['adults'] + (array_key_exists("children", $payment_info) ? $payment_info['children'] : 0) + (array_key_exists("infants", $payment_info) ? $payment_info['infants'] : 0) . ($item['Type'] == 'oneway' ? ' MC' : ' KH'); ?></span>
-						<p><?php echo $payment_info['adults'] . ' NL'; ?></p>
-						<p><?php echo (array_key_exists("children", $payment_info) ? $payment_info['children'] . ' TE' : ''); ?>
-						</p>
-						<p><?php echo (array_key_exists("infants", $payment_info) ? $payment_info['infants'] . ' EB' : ''); ?>
-						</p>
-					</div>
-					<div class="col l-1">
-						<span><?= number_format($payment_info['total_price'], 0, ".", ".") ?></span>
-					</div>
-					<div class="col l-4">
-						<?php foreach ($flight_detail['flight_detail'] as $flight) { ?>
-						<p><?= $flight['carrierCode'] . $flight['number'] . ' ' . $flight['date'] . ' ' . $flight['from'] . ' ' . $flight['to'] . ' ' . $flight['departure_time'] . ' ' . $flight['arrival_time'] ?>
-						</p>
-						<?php } ?>
-					</div>
-					<div class="col l-2">
-						<p><?= $payment_info['contact_name'] ?></p>
-						<p><?= $payment_info['contact_phone'] ?></p>
-					</div>
-					<div class="col l-2">
-						<span><?= $item['Booking_DateTime'] ?></span>
-					</div>
-					<div class="status col l-1">
-						<span payment="<?php echo $item['Status'] == 0 ? 'false' : 'true'; ?>"
-							value='<?= $item['Order_ID'] ?>'><?php echo $item['Status'] == 0 ? 'Chưa TT' : 'Đã TT'; ?></span>
-					</div>
-				</a>
-				<?php endforeach; ?>
-			</div>
-
+			</a>
+			<?php endforeach; ?>
 		</div>
-	</section>
-	<script>
-	$(document).ready(function() {
 
-		$(document).on("click", ".status span[payment='false']", function() {
-			var pay = confirm("Xác nhận đã thanh toán");
-			if (pay == true) {
-				$.ajax({
-					type: "POST",
-					url: "<?= base_url() ?>admin/order/updatestatus",
-					data: {
-						order_id: $(".status span[payment='false']").attr('value')
-					},
-					dataType: "json",
-					success: function(response) {
-						if (response == "Xác nhận thành công") {
-							toastr["success"](response);
-						} else {
-							toastr["error"](response);
-						}
+	</div>
+</section>
+<script>
+$(document).ready(function() {
+
+	$(document).on("click", ".status span[payment='false']", function() {
+		var pay = confirm("Xác nhận đã thanh toán");
+		if (pay == true) {
+			$.ajax({
+				type: "POST",
+				url: "<?= base_url() ?>admin/order/updatestatus",
+				data: {
+					order_id: $(".status span[payment='false']").attr('value')
+				},
+				dataType: "json",
+				success: function(response) {
+					if (response == "Xác nhận thành công") {
+						toastr["success"](response);
+					} else {
+						toastr["error"](response);
 					}
-				});
-				$(this).removeAttr("payment");
-				$(this).text("Đã TT");
-			}
-		});
-
+				}
+			});
+			$(this).removeAttr("payment");
+			$(this).text("Đã TT");
+		}
 	});
-	</script>
+
+});
+</script>
--- application/views/admin/order_detail.php
+++ application/views/admin/order_detail.php
@@ -1,1 +1,234 @@
-<h2>Chi tiết hoá đơn <span>#123456</span> </h2>
+<h2 class="title-order-detail">Chi tiết hoá đơn <span>#123456</span> </h2>
+<section class="order-detail">
+	<div class="grid">
+		<div class="row">
+			<div class="col l-8 info-flight">
+				<div class="title">
+					<div class="from-to">
+						<span>SGN</span>
+						<i class="fas fa-long-arrow-alt-right"></i>
+						<span>HAN</span>
+					</div>
+				</div>
+				<div class="class">
+					<i class="fas fa-plane"></i>
+					<span>Economy Class</span>
+					<span>Một chiều</span>
+					<span>Chưa thanh toán</span>
+				</div>
+
+				<div class="flight-wrap">
+					<div class="base-from-to">
+						<h6>SGN</h6>
+						<p class="airport">Tan Son Nhat International Airport</p>
+					</div>
+					<i class="fas fa-plane"></i>
+					<div class="base-from-to">
+						<h6>HAN</h6>
+						<p class="airport">Noi Bai International Airport</p>
+					</div>
+				</div>
+				<div class="depart-arrive">
+					<div>
+						<p>Khởi hành</p>
+						<span>31/05/2021</span>
+						<span>12h00</span>
+					</div>
+					<i class="fas fa-plane"></i>
+					<div>
+						<p>Đến</p>
+						<span>31/05/2021</span>
+						<span>15h00</span>
+					</div>
+				</div>
+			</div>
+			<div class="col l-4 info-user">
+				<div class="title">
+					<a href="#">
+						<i class="fas fa-long-arrow-alt-left"></i>
+						Trở lại
+					</a>
+				</div>
+
+				<div class="code">
+					<div class="order-code">
+						<span>Mã hoá đơn: </span>
+						<span>#123456</span>
+					</div>
+					<div class="order-datetime">
+						<span>Ngày / Thời gian đặt:</span>
+						<span>12/5/2021</span>
+						<span>12h000</span>
+					</div>
+				</div>
+				<div class="traveller-info">
+					<h6>Thông tin khách hàng đặt vé</h6>
+					<div class="row heading">
+						<div class="col l-8">
+							Tên
+						</div>
+						<div class="col l-4">
+							Độ tuổi
+						</div>
+					</div>
+					<div class="items">
+						<div class="row body">
+							<div class="col l-8">
+								Nguyễn Trung Hiếu
+							</div>
+							<div class="col l-4">
+								Người lớn
+							</div>
+						</div>
+						<div class="row body">
+							<div class="col l-8">
+								Nguyễn Trung Hiếu
+							</div>
+							<div class="col l-4">
+								Em bé
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<div class="row flight-detail">
+			<div class="col l-12 flight-detail-wrap">
+				<h6>Chi tiết chuyến bay</h6>
+				<div class="row heading">
+					<div class="col l-2">
+						Mã chuyến bay
+					</div>
+					<div class="col l-2">
+						Cất cánh
+					</div>
+					<div class="col l-2">
+						Điểm đi
+					</div>
+					<div class="col l-2">
+						Điểm đến
+					</div>
+					<div class="col l-2">
+						Thời gian khởi hành
+					</div>
+					<div class="col l-2">
+						Thời gian Đến nơi
+					</div>
+				</div>
+				<div class="items">
+					<div class="row body">
+						<div class="col l-2">
+							VN225
+						</div>
+						<div class="col l-2">
+							5/5/2021
+						</div>
+						<div class="col l-2">
+							SGN
+						</div>
+						<div class="col l-2">
+							HAN
+						</div>
+						<div class="col l-2">
+							15:25
+						</div>
+						<div class="col l-2">
+							07:45
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col l-8 info-contact">
+			<h6>Thông tin liên hệ</h6>
+			<div class="row">
+				<div class="col l-6">
+					<p>Tên:</p>
+					<p>Nguyễn Trung Hiếu</p>
+				</div>
+				<div class="col l-6">
+					<p>Email:</p>
+					<p>hieucot69@gmail.com</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col l-6">
+					<p>Số điện thoại:</p>
+					<p>0582 187 188</p>
+				</div>
+				<div class="col l-6">
+					<p>Địa chỉ:</p>
+					<p>Ấp 1, Xá Bàu cạn, Huyện Long Thành, Tỉnh Đồng Nai</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col l-6">
+					<p>Phương thức thanh toán:</p>
+					<p>Thanh toán online</p>
+				</div>
+				<div class="col l-6">
+					<p>Ghi chú:</p>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis necessitatibus
+						vel illum magni voluptas iure ullam voluptatem aspernatur, praesentium omnis,
+						pariatur ratione eaque ut deserunt id. Vel architecto repudiandae ullam!</p>
+				</div>
+			</div>
+		</div>
+		<div class="col l-4 payment-detail">
+			<h6>Chi tiết thanh toán</h6>
+			<div class="row heading">
+				<div class="col l-6">
+					Tên
+				</div>
+				<div class="col l-2">
+					SL
+				</div>
+				<div class="col l-4">
+					Giá
+				</div>
+			</div>
+			<div class="items">
+				<div class="row body">
+					<div class="col l-6">
+						Nguyễn Trung Hiếu
+					</div>
+					<div class="col l-2">
+						1
+					</div>
+					<div class="col l-4">
+						1.700.000
+					</div>
+				</div>
+				<div class="row body">
+					<div class="col l-6">
+						Nguyễn Trung Hiếu
+					</div>
+					<div class="col l-2">
+						1
+					</div>
+					<div class="col l-4">
+						1.300.000
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row footer">
+				<div class="bill col l-6">
+					Tổng hoá đơn:
+				</div>
+				<div class="col l-6 total-price">
+					<span>3.000.000</span>
+					<span>VND</span>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	</div>
+
+</section>
