386b6f2727d2902fedfb7dc3b8bbe936d32dc354
Lộc Trần <hoangloc1307@gmail.com>
2022-08-11 20:32:57 +0700

    First commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/baocao/';
+$config['base_url'] = 'http://localhost/lhd_flight_ticket/';
 
 /*
 |--------------------------------------------------------------------------
